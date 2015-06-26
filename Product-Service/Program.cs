bb1bad771d7e14176147bc54031392704abbb398
iancooper <ian_hammond_cooper@yahoo.co.uk>
2015-06-26 19:05:36 +0100

    Running, but non-functional, product service

--- Product-Service/Program.cs
+++ Product-Service/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Product_Service
+﻿using Product_Service.Adapters.Service;
+using Topshelf;
+
+namespace Product_Service
 {
     class Program
     {
         static void Main(string[] args)
         {
+           HostFactory.Run(x => x.Service<ProductService>(sc =>
+           {
+               sc.ConstructUsing(() => new ProductService());
+
+                    // the start and stop methods for the service
+                    sc.WhenStarted((s, hostcontrol) => s.Start(hostcontrol));
+               sc.WhenStopped((s, hostcontrol) => s.Stop(hostcontrol));
+
+                    // optional, when shutdown is supported
+                    sc.WhenShutdown((s, hostcontrol) => s.Shutdown(hostcontrol));
+           }));
+
         }
     }
 }
